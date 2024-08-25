a54e17089de421de46a9524221bfb0ba86a4ec26
ArabuliGiorgi <qartveli.giorgi.2003@gmail.com>
2024-08-25 21:49:42 +0400

    testing copy

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ copy.addEventListener('click', () => {
     if (navigator.clipboard && navigator.clipboard.writeText) {
         navigator.clipboard.writeText(toCopy).then(() => {
             copy_text.display = "block";
+            console.log("copy success");
         }).catch((err) => {
             console.log("Copy error: ", err);
         });
